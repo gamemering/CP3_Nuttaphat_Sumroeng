2c5077c277bc44ac750d62dc775b09141f6ff1ae
gamemering <59176183+gamemering@users.noreply.github.com>
2020-08-08 11:15:02 +0700

    Update CP3_Nuttaphat_S.py

--- CP3_Nuttaphat_S.py
+++ CP3_Nuttaphat_S.py
@@ -4,8 +4,7 @@ ICS = 75.5
 CP = 62.5
 
 print("---Your Score---")
-print("oundation English =" ,FE,"p.")
-print("General Business =" ,GB,"p.")
-print("Introduction to Computer Systems =" ,ICS,"p.")
-print("Computer Programming =" ,CP,"p.")
-
+print("oundation English                 =" ,FE,"p.")
+print("General Business                  =" ,GB,"p.")
+print("Introduction to Computer Systems  =" ,ICS,"p.")
+print("Computer Programming              =" ,CP,"p.")
