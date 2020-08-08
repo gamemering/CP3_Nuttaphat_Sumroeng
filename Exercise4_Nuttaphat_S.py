f811a1832e899ab78045f0b1e8aa49c1fb8105de
gamemering <59176183+gamemering@users.noreply.github.com>
2020-08-08 11:33:10 +0700

    Update Exercise4_Nuttaphat_S.py

--- Exercise4_Nuttaphat_S.py
+++ Exercise4_Nuttaphat_S.py
@@ -3,8 +3,11 @@ GB = 70
 ICS = 75.5
 CP = 62.5
 
-print("---Your Score---")
-print("oundation English                 =" ,FE,"p.")
-print("General Business                  =" ,GB,"p.")
-print("Introduction to Computer Systems  =" ,ICS,"p.")
-print("Computer Programming              =" ,CP,"p.")
+print("----------------Your Score-----------------")
+print("                                          ")
+print("oundation English                 =",FE,"p.")
+print("General Business                  =",GB,"p.")
+print("Introduction to Computer Systems  =",ICS,"p.")
+print("Computer Programming              =",CP,"p.")
+print("                                          ")
+print("----------------Good Luck------------------")
