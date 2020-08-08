3a6df1c4f8941d6863537c299f789012e48ea3ca
gamemering <59176183+gamemering@users.noreply.github.com>
2020-08-08 11:13:41 +0700

    Create CP3_Nuttaphat_S.py

---
+++ CP3_Nuttaphat_S.py
@@ -0,0 +1,11 @@
+FE = 60
+GB = 70
+ICS = 75.5
+CP = 62.5
+
+print("---Your Score---")
+print("oundation English =" ,FE,"p.")
+print("General Business =" ,GB,"p.")
+print("Introduction to Computer Systems =" ,ICS,"p.")
+print("Computer Programming =" ,CP,"p.")
+
