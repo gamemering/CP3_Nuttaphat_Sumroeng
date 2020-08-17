40795f26d88a3a06dba157fe855633c5910df610
gamemering <59176183+gamemering@users.noreply.github.com>
2020-08-17 13:32:58 +0700

    Create Exercise5_1_Nuttaphat_P.py

---
+++ Exercise5_1_Nuttaphat_P.py
@@ -0,0 +1,12 @@
+number1 = float(input("First Number : "))
+number2 = float(input("Second Number : "))
+
+result1 = number1 + number2
+result2 = number1 - number2
+result3 = number1 * number2
+result4 = number1 / number2
+
+print(number1,"+",number2,"=",result1)
+print(number1,"-",number2,"=",result2)
+print(number1,"*",number2,"=",result3)
+print(number1,"/",number2,"=",result4)
