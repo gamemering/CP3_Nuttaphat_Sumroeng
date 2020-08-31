3c2c7b14be8332ea32f56a3a8170c44e2a01d6a2
gamemering <59176183+gamemering@users.noreply.github.com>
2020-08-31 18:27:37 +0700

    Create FunctionAddNumber.py

---
+++ FunctionAddNumber.py
@@ -0,0 +1,13 @@
+def addNumberPlus(x,y):
+    print(x+y)
+def addNumberMinus(x,y):
+    print(x-y)
+def addNumberMultiply(x,y):
+    print(x*y)
+def addNumberDivide(x,y):
+    print(x/y)
+
+addNumberPlus(10,5)
+addNumberMinus(10,5)
+addNumberMultiply(10,5)
+addNumberDivide(10,5)
